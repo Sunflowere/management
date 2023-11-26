c7d79250075bdd70d8691b61f6abbaf764060e70
hongtao.liu <1844738656@qq.com>
2023-11-26 21:48:34 +0800

    import Element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <el-button type="blue">test</el-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>

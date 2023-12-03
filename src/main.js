183982f8207af773c356caebc8d36cc244018892
hongtao.liu <1844738656@qq.com>
2023-12-03 19:19:09 +0800

    fram ok

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import './assets/global.css';
 Vue.config.productionTip = false
 
-Vue.use(ElementUI, {size: "small"});
+Vue.use(ElementUI, {size: "mini"});
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,11 +12,6 @@
           <template slot="title"><i class="el-icon-message"></i>
             <span slot="title">导航一</span>
           </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
           <el-menu-item-group title="分组2">
             <el-menu-item index="1-3">选项3</el-menu-item>
           </el-menu-item-group>
@@ -29,14 +24,6 @@
           <template slot="title"><i class="el-icon-menu"></i>
             <span slot="title">导航二</span>
           </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
           <el-submenu index="2-4">
             <template slot="title">选项4</template>
             <el-menu-item index="2-4-1">选项4-1</el-menu-item>
@@ -46,14 +33,6 @@
           <template slot="title"><i class="el-icon-setting"></i>
             <span slot="title">导航三</span>
           </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
           <el-submenu index="3-4">
             <template slot="title">选项4</template>
             <el-menu-item index="3-4-1">选项4-1</el-menu-item>
@@ -78,18 +57,36 @@
       </el-header>
 
       <el-main>
-        <div style="padding: 10px 0;">
+        <div>
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
+        <div style="margin: 10px 0;">
           <el-input style="width: 200px;" placeholder="请输入名称" suffix-icon="el-icon-search"></el-input>
           <el-input style="width: 200px;" placeholder="请输入邮箱" suffix-icon="el-icon-message"></el-input>
           <el-input style="width: 200px;" placeholder="请输入地址" suffix-icon="el-icon-position"></el-input>
           <el-button class="ml-5" type="primary">搜索</el-button>
         </div>
-        <el-table :data="tableData" border stripe >
+        <div style="margin: 10px 0;">
+          <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+          <el-button type="danger">批量删除 <i class="el-icon-remove-outline"></i></el-button>
+          <el-button type="primary">导入 <i class="el-icon-bottom"></i></el-button>
+          <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
+        </div>
+        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
           <el-table-column prop="date" label="日期" width="140">
           </el-table-column>
           <el-table-column prop="name" label="姓名" width="120">
           </el-table-column>
           <el-table-column prop="address" label="地址">
+          </el-table-column>
+          <el-table-column>
+            <template slot-scope="scope">
+              <el-button type="primary">编辑<i class="el-icon-edit"></i></el-button>
+              <el-button type="danger">删除<i class="el-icon-remove-outline"></i></el-button>
+            </template>
           </el-table-column>
         </el-table>
         <div style="padding: 10px 0;">
@@ -110,15 +107,16 @@ export default {
   data() {
     const item = {
       date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
+      name: '唐小虎',
+      address: '上海市松江区文汇路 300 弄'
     };
     return {
       tableData: Array(10).fill(item),
       collapseBtnClass: 'el-icon-s-fold',
       isCollapse: false,
       sideWidth: 200,
-      logoTextShow: true
+      logoTextShow: true,
+      headerBg: 'headerBg'
     }
   },
   methods: {
@@ -138,3 +136,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.headerBg {
+  background-color: rgb(232, 228, 228) !important;
+}
+</style>
